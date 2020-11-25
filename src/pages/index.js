7a4bceca9d29570513862026c0ae0cbbdba05120
kol845 <pebo-hamza@hotmail.com>
2020-11-25 12:49:49 +0100

    Created API logic and imported graph

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import React from "react"
 import Header from "./../components/Header"
 import Button from "./../components/Button"
+import Graph from "./../components/Graph"
 import "./../css/reset.css";
 import "./../css/home.css";
 import Bg from "../images/map.png"
-import GrapthImg from "../images/graph.png"
+import GraphImg from "../images/graph.png"
+import api from "./../logic/api.js";
 
 export default function Home() {
+  let stockData = api.cryptoAPI("1m")
   return(
     <React.Fragment>
     
@@ -17,7 +20,8 @@ export default function Home() {
       <Header />  
 
       <div className="graph">
-        <img className="graphImg" src={GrapthImg} />
+        {/* <img className="graphImg" src={GraphImg} /> */}
+        <Graph/>
       </div>
 
       <div className="button-container">
