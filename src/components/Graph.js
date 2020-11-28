a37542e26bf44aa9829605f12a97fdda0408b481
kol845 <pebo-hamza@hotmail.com>
2020-11-28 18:49:33 +0100

    Unifying api dates

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,34 +1,66 @@
 
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { VictoryChart, VictoryZoomContainer, VictoryLine, VictoryBrushContainer, VictoryAxis, VictoryTheme } from 'victory';
+import api from "./../logic/api.js";
+
+
 
 export default function Graph(props) {
-    console.log(props)
+    const timeframe = props.timeframe;
+    const [isLoading, setLoading] = useState(true);
+    const [cryptoData, setCryptoData] = useState([]);
+    const [stockData, setStockData] = useState([]);
 
+    async function fetchData(fetchFunction, callbackFunction) { // Fetches from the API
+        await fetchFunction(timeframe).then((response) =>{
+            callbackFunction(response)
+        });
+    }
 
+    useEffect(() => {
+        setLoading(true);
+        fetchData(api.cryptoAPI, setCryptoData);
+        fetchData(api.stockAPI, setStockData);
+        setLoading(false);
+    }, []); // This empty array fixed the bug where useEffect() was constantly rerunning.
+    if(isLoading){ // If we are still loading the data from API
+        return(
+            <div style={{width: "60vw", margin: "auto", fontSize:"150px"}}>
+                Loading...
+            </div>
+        )
+    }
+ 
     return(
-    <React.Fragment>
-        
-        <div style={{width: "60vw", margin: "auto"}}>
-            <VictoryChart
-                width={1030}
-                height={600}
-                theme={VictoryTheme.material}
-            >
-            <VictoryLine
-                
-                style={{
-                data: { stroke: "#c43a31" },
-                parent: { border: "1px solid #ccc"}
-                }}
-                // data={props.children}
-            />
-            </VictoryChart>
-        </div>
-
-            
+        <React.Fragment>
+            {/* {console.log(cryptoData)}
+            {console.log(stockData)} */}
+            <div style={{width: "60vw", margin: "auto"}}>
+                <VictoryChart
+                    width={1030}
+                    height={600}
+                    theme={VictoryTheme.material}
+                >
+                <VictoryLine
+                    style={{
+                    data: { stroke: "#FFC700" },
+                    parent: { border: "1px solid #ccc"}
+                    }}
+                    data={cryptoData}
+                />
+                <VictoryLine
+                    style={{
+                    data: { stroke: "#107C2E" },
+                    parent: { border: "1px solid #ccc"}
+                    }}
+                    data={stockData}
+                />
+                </VictoryChart>
+            </div>     
         </React.Fragment>
-);}
+    )
+    
+;}
 
   
 
--- src/logic/api.js
+++ src/logic/api.js
@@ -3,20 +3,58 @@
 //https://siblisresearch.com/data/us-stock-market-value/#:~:text=The%20total%20market%20capitalization%20of,9%2F30%2F2020).
 const willshireRatio = 1055; // Miljon Dollars in stock market (1 Willshire point = 1055 miljon $ in stock market)
 
+const addWeekends = (data) =>{
+    console.log(data) // LOL DATE API USES 5 WHILE STOCK API USES 05
+    let date = new Date();
+    let prevDate = null;
+    let dateArray = [];
+    let newData = []
+    let size = data.length;
+    date.setDate((date.getDate()-size))
+    for(let i = 0;i<size;i++){
+        let fDate = ""+date.getFullYear() +"-"+ (date.getMonth()+1) +"-"+ date.getDate();// date format
+        dateArray.push(fDate);
+        date.setDate((date.getDate()+1))
+    }
+    console.log(dateArray)
+    let count = 0;
+    data.forEach((element, i) => {
+        if(element.datetime == dateArray[count]){
+            count++;
+            newData.push(element)
+        }
+        else if (count>0){
+            newData.push({...newData[count-1], "datetime":dateArray[count]})
+            count++;
+            newData.push({...newData[count-1], "datetime":dateArray[count]})
+            count++;
+            newData.push(element)
+            count++;
+        }
+
+    });
+    console.log(newData);
+}
+
 const skimStockData = (data) =>{
     let array = [];
+    data.values = data.values.reverse();
+    data = addWeekends(data.values);
+    let prevVal = data.values[0].close;
     for (const [key, value] of Object.entries(data.values)) {
         
-        value = {"datetime":value.datetime,"close": value.close}
+        value = {"x":value.datetime,"y": ((value.close-prevVal)/prevVal)*100}
         array.push(value);
     }
     return array;
 }
 const skimCryptoData = (data)=>{
+    // console.log(data);
     let array = [];
+    // data = data.reverse()
     let prevVal = data[0].volume;
     for (const [key, value] of Object.entries(data)) {
-        value = {"timestamp":value.timestamp.slice(0,-10),"volume": ((value.volume-prevVal)/prevVal)*100}
+        value = {"x":value.timestamp.slice(0,-10),"y": ((value.volume-prevVal)/prevVal)*100}
         array.push(value);
     }
     return array;
@@ -36,24 +74,25 @@ const stockAPI = async (timeframe) => {
     } else if (timeframe.toLowerCase() === "1y"){        
         outputSize = 30*12;
     }
-
     
     const url = "https://api.twelvedata.com/time_series?symbol=W5000&interval=1day&outputsize="+outputSize+"&apikey=a467faa10a8d4c369bee33d8f2e07daf"
     let response = await fetch(url)
         .then(response => (
             response.json())
             .then(data => {
-                if(data.code!=200){
+                if(data.status == "ok"){
+                    return skimStockData(data)
+                }else{
                     console.log("API error: ");
                     console.log(data)
-                }else{
-                    skimStockData(data)
                 }
             })
             )
         .catch(error => {
             console.log("HOLY SHIT, Error occured! Run for your life!");
+            console.log(error);
         });
+    // console.log(response);
     return response;
 };
 
@@ -72,24 +111,28 @@ async function cryptoAPI (timeframe) {
     let url;
 
     if(timeframe.toLowerCase() === "7d"){
-        date.setDate(today.getDate()-6);
+        date.setDate(today.getDate()-7);
     } else if (timeframe.toLowerCase() === "1m"){        
-        date.setDate(today.getDate()-30);
+        date.setDate(today.getDate()-31);
     } else if (timeframe.toLowerCase() === "6m"){        
-        date.setDate(today.getDate()-30*6);
+        date.setDate(today.getDate()-31*6);
     } else if (timeframe.toLowerCase() === "1y"){        
-        date.setDate(today.getDate()-30*12);
+        date.setDate(today.getDate()-31*12);
     }
     startDate = ""+date.getFullYear() +"-"+ (date.getMonth()+1) +"-"+ date.getDate();
     url = "https://api.nomics.com/v1/volume/history?key=cb20c6e35d1b2f873402d249f2c145cf&start="+startDate+"T00%3A00%3A00Z&end="+endDate+"T00%3A00%3A00Z&convert=USD"
     let response = await fetch(url)
     .then(response => (
         response.json())
-        .then(data => skimCryptoData(data))
+        .then(data => {   
+            return skimCryptoData(data)
+        })
         )
     .catch(error => {
         console.log("HOLY SHIT, Error occured! Run for your life!");
+        console.log(error);
     });
+    // console.log(response)
     return response;
 };
 export default {
